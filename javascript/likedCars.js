880f43f7dd80ffbc0c84e7bcaef880e6d2e8d435
shaolaser-123 <wcdnmpsh64@privaterelay.appleid.com>
2026-05-08 21:51:23 +0200

    reset
    reset

--- javascript/likedCars.js
+++
@@ -1,1 +0,0 @@
-export const likedCars = JSON.parse(localStorage.getItem('likedCars')) || [];
--- javascript/theJah.js
+++
@@ -1,6 +0,0 @@
-const dropdown = document.querySelector('.dropdown');
-const offsmenu = document.querySelector('.off-s-menu');
-dropdown.addEventListener('click', ()=>{
-    dropdown.classList.toggle('active');
-    offsmenu.classList.toggle('active');
-});
